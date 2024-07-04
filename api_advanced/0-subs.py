9307b8df70e46f602fc94b962f7b618cf0426051
oreste-abizera <oresteabizera11@gmail.com>
2024-07-04 18:37:59 +0200

    ref: update API endpoint [IN PROGRESS]

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     """
     number of subscribers
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://oauth.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}  # avoid Too Many Requests error
 
     response = requests.get(url, headers=headers, allow_redirects=False)
--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ import requests
 
 def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    url = 'https://oauth.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
